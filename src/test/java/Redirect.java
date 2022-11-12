0f9b17d6b05fa7f52684f00810bcc110e24d37b1
bondarenkoolga <bondarenkoolga@gmail.com>
2022-11-12 23:54:34 +0600

    Redirect

--- src/test/java/Redirect.java
+++ src/test/java/Redirect.java
@@ -1,11 +1,8 @@
 import io.restassured.RestAssured;
-import io.restassured.http.Header;
-import io.restassured.http.Headers;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.soap.SAAJResult;
+
 import java.util.HashMap;
 import java.util.Map;
 
